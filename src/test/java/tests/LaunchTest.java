e3c203f8075da3266032fd9977d84a9f1b65c221
OlhaKeller <kellerolha@yahoo.com>
2022-05-11 20:25:08 +0200

    best

---
+++ src/test/java/tests/LaunchTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import configuration.AppiumConfiguration;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AppiumConfiguration {
+
+    @Test
+    public void launchTest(){
+
+        
+    }
+}
+
