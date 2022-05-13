b445ab7619bb0b489ca33a0327c6e44f77b9307f
OlhaKeller <kellerolha@yahoo.com>
2022-05-13 16:14:53 +0200

    newRevision

--- src/main/java/configuration/AppiumConfiguration.java
+++ src/main/java/configuration/AppiumConfiguration.java
@@ -1,6 +1,5 @@
 package configuration;
 
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,28 +8,30 @@ import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+//          platformName: "Android",
+//        deviceName: "MyNex",
+//        platformVersion: "8.0",
+//        appPackage: "com.example.svetlana.scheduler",
+//        appActivity: ".presentation.splashScreen.SplashScreenActivity"
 
-//"platformName": "Android",
-//      "deviceName": "Nex",
-//    "platformVersion": "8.0",
-//  "appPackage": "com.example.svetlana.scheduler",
-//"appActivity": ".presentation.splashScreen.SplashScreenActivity"
 
-public class AppiumConfiguration{
-    protected static AppiumDriver<MobileElement> driver;
+public class AppiumConfiguration {
+    protected  static  AppiumDriver <MobileElement> driver;
 
     @BeforeSuite
-    public void setUp ()throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();//Object
-        //jk nazuvaetsy divice
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName","Nex");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
-        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+        capabilities.setCapability( "appActivity", ".presentation.splashScreen.SplashScreenActivity");
         capabilities.setCapability("automationName","Appium");
 
-        driver=new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver=new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+
+
     }
     @AfterSuite
     public void tearDown(){
